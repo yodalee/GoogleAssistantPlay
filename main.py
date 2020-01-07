797fb08591b3c45f140fcd88240b75efbce1774d
yodalee <lc85301@gmail.com>
2020-01-07 21:10:42 +0800

    add webhook response to default welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import json
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -8,6 +9,25 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    """Handle google action webhook
+    :returns: response text
+    """
+    data = request.get_json(silent=True, force=True)
+    print("Request:{}".format(json.dumps(data, indent=2)))
+
+    res = dispatchHandler(data)
+    return res
+
+def dispatchHandler(data):
+    action_name = data.get("queryResult").get("intent").get("displayName")
+    if action_name == "Default Welcome Intent":
+        text = "I have a number between 1 and 50. Can you guess it?"
+        print("Response:{}".format(text))
+        reply = { "fulfillmentText": text }
+        return jsonify(reply)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
