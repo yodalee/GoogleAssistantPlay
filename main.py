0c921001eed804ba5cbbd077fe9c10dd859c354e
yodalee <lc85301@gmail.com>
2020-07-13 23:58:47 +0800

    complete db session record by pickledb

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, make_response
+import pickledb
 import json
+import random
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
+db = pickledb.load('simple.db', False)
 
 @app.route('/')
 def hello():
@@ -22,9 +25,38 @@ def webhook():
     return res
 
 def dispatchHandler(data):
+    session = data.get("session")
+
     action_name = data.get("queryResult").get("intent").get("displayName")
     if action_name == "Default Welcome Intent":
-        text = "I have a number between 1 and 50. Can you guess it?"
+        low = 0
+        high = 5
+        target = random.randint(low+1, high-1)
+        db.set(session, (low, target, high))
+        text = "I have a number between {} and {}. Can you guess it?".format(low, high)
+        print("Response:{}".format(text))
+        reply = { "fulfillmentText": text }
+        return jsonify(reply)
+
+    elif action_name == "GuessNumber":
+        pair = db.get(session)
+        guessnum = int(data.get("queryResult").get("parameters").get("number"))
+        minnum, target, maxnum = pair
+        if guessnum > maxnum or guessnum < minnum:
+            text = "Are you sure? I said a number between {} and {}".format(minnum, maxnum)
+        else:
+            if guessnum == target:
+                event = "User_number_match"
+                reply = { "followup_event_input" : { "name" : "User_number_match" } }
+                return jsonify(reply)
+            else:
+                if guessnum < target:
+                    minnum = guessnum
+                else:
+                    maxnum = guessnum
+                text = "A number between {} and {}. Keep guess.".format(minnum, maxnum)
+                db.set(session, (minnum, target, maxnum))
+
         print("Response:{}".format(text))
         reply = { "fulfillmentText": text }
         return jsonify(reply)
